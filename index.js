3b52cd0445c0c11d5e38a152e70e79657b6e42e2
Leissan Khan <leissan.khan@gmail.com>
2022-03-30 20:02:05 -0400

    commit tree 71a13280758c18f3441ecc96de01010b62baa96e

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Your code here
+function createEmployeeRecord (array) {
+    return {
+        firstName: array[0],   
+        familyName: array[1],   
+        title: array[2], 
+        payPerHour: array[3],
+        timeInEvents: [],
+        timeOutEvents: []
+     }
+    }
+
+function createEmployeeRecords(arrayOfArrays) {
+    return array.map ((record) => createEmployeeRecord(record))
+}
+
+const createTimeInEvent = function (dateStamp){
+    const [date, hour] = dateStamp.split(" ");
+    const inEvent = {
+        type: "TimeIn",
+        hour: parseInt (hour),
+        date: date,
+    }
+    this.timeInEvents.push (inEvent); 
+    return this;
+}
